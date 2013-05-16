960e5b50957159f62a2d01492e9eaa0600298465
Donald Piret <donald@donaldpiret.com>
2013-05-16 09:40:04 +0800

    Do a git reset hard before the git pull

--- manifests/update.pp
+++ manifests/update.pp
@@ -7,6 +7,13 @@ define dotfiles::update(
   ) {
 
   if (!$single_pull) {
+    exec { "reset ${gituser} dot file changes for ${titls}":
+      cwd		=> "${cwd}",
+      command	=> "git reset --hard HEAD",
+      user    => "${title}",
+      require => Package['boxen/brews/git'],
+    }
+
     exec { "update ${gituser} dotfiles for ${title}":
       cwd     => "${cwd}",
       command => $rebase ? {true  => "git pull --rebase", false => "git pull", },
